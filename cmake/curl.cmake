b3d1f34879c02644e8bbb5ef3fe1e808d38b8d26
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2024-11-13 23:23:55 +0200

    setup ccache (#16)
    * setup sccache
    * organize curl.cmake
    * add cmake caching

--- cmake/curl.cmake
+++ cmake/curl.cmake
@@ -1,14 +1,14 @@
 FetchContent_Declare(curl URL https://curl.se/download/curl-8.11.0.tar.gz)
 
 set(BUILD_STATIC_LIBS ON)
+set(HTTP_ONLY ON)
 set(BUILD_SHARED_LIBS OFF)
-set(BUILD_TESTING OFF)
 set(BUILD_EXAMPLES OFF)
 set(USE_LIBIDN2 OFF)
 set(BUILD_CURL_EXE OFF)
 set(CURL_USE_LIBPSL OFF)
 set(CURL_USE_LIBSSH2 OFF)
-set(HTTP_ONLY ON)
+set(BUILD_TESTING OFF)
 
 if(WIN32)
     # Native https on Windows
